b7b8455c648b5ace567a5d63646b17c0570f3dbb
kukinpower <thekukinpower@gmail.com>
2020-11-03 22:16:52 +0300

    doubly bubble sort

--- lesson05/E_double_bubble_sort.cpp
+++ lesson05/E_double_bubble_sort.cpp
@@ -21,11 +21,15 @@ int		main(void)
 	{
 		cin >> arr[i];
 	}
-//	n = 10;
-//	int arr[10] = {-4, -79, -17, -12, 94, 65, -56, -79, -7, 39};
+/*
+ * n = 10;
+ * int arr[10] = {-4, -79, -17, -12, 94, 65, -56, -79, -7, 39};
+ * n = 15;
+ * int arr[15] = {78, -57, -54, -61, -10, 42, -19, -92, 0, -23, -27, -3, 4, -41, -94};
+ */
+
 	bool isSorted = false;
 	int tmp;
-
 
 	int j = 0;
 	while (!isSorted)
@@ -33,29 +37,19 @@ int		main(void)
 		isSorted = true;
 		for (int i = 0; i < n - 1; i++)
 		{
-			if (arr[i] < 0)
+			if (arr[i] >= 0)
 			{
 				j = i + 1;
-				while (arr[j] >= 0 && j < n)
+				while (arr[j] < 0 && j < n - 1)
 				{
-					j++;
-				}
-				if (arr[j] < 0 && arr[i] < arr[j])
-				{
-					tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-					isSorted = false;
-					printArr(arr, n);
-//					i++;
-				}
-
-			}
-			else if (arr[i] >= 0)
-			{
-				j = i + 1;
-				while (arr[j] < 0 && j < n)
-				{
+					if (arr[j + 1] < 0 && arr[j] < arr[j + 1])
+					{
+						tmp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = tmp;
+						isSorted = false;
+						printArr(arr, n);
+					}
 					j++;
 				}
 				if (arr[j] >= 0 && arr[i] > arr[j])
@@ -65,25 +59,33 @@ int		main(void)
 					arr[j] = tmp;
 					isSorted = false;
 					printArr(arr, n);
-//					i++;
+				}
+			}
+			else if (arr[i] < 0)
+			{
+				j = i + 1;
+				while (arr[j] >= 0 && j < n - 1)
+				{
+					if (arr[j + 1] >= 0 && arr[j] > arr[j + 1])
+					{
+						tmp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = tmp;
+						isSorted = false;
+						printArr(arr, n);
+					}
+					j++;
+				}
+				if (arr[j] < 0 && arr[i] < arr[j])
+				{
+					tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+					isSorted = false;
+					printArr(arr, n);
+					i = j - 1;
 				}
 			}
 		}
 	}
 }
-
-/*
--4 -17 -79 -12 94 65 -56 -79 -7 39
--4 -17 -12 -79 94 65 -56 -79 -7 39
--4 -17 -12 -79 65 94 -56 -79 -7 39
--4 -17 -12 -56 65 94 -79 -79 -7 39
--4 -17 -12 -56 65 94 -79 -7 -79 39
--4 -17 -12 -56 65 39 -79 -7 -79 94
--4 -12 -17 -56 65 39 -79 -7 -79 94
--4 -12 -17 -56 39 65 -79 -7 -79 94
--4 -12 -17 -56 39 65 -7 -79 -79 94
--4 -12 -17 -7 39 65 -56 -79 -79 94
--4 -12 -7 -17 39 65 -56 -79 -79 94
--4 -7 -12 -17 39 65 -56 -79 -79 94
-
-*/
